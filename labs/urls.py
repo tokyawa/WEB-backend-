4b82c57293d3a770877e820b43501f1780492cee
Илья Салищев <148629991+tokyawa@users.noreply.github.com>
2024-12-16 00:16:57 +0300

    Update urls.py

--- labs/urls.py
+++ labs/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from bmstu import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('info/<int:district_id>/', views.info, name='district_info'),
-    path('orderscart/', views.orders_cart, name='orderscart'),
-    path('add_to_cart/<int:district_id>/', views.add_to_cart, name='add_to_cart'),
-    path('clear_cart/', views.clear_cart, name='clear_cart'),
+    path('', views.district_list, name='district_list'),
+    path('district_about/<int:id>/', views.district_about, name='district_about'),
+    path('zone/<int:zone_id>/', views.zone, name='zone')
+
 ]
